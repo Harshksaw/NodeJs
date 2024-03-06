4381b0f1bd9c9bc2d75ea8150e20d3cf8b56015c
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-06 07:18:26 +0530

    Serverless Aws

---
+++ serveless-nodejs-api/src/index.js
@@ -0,0 +1,27 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
